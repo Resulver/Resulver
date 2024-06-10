874cd21e933415a282fa013f1f1f173145c453da
Mobin Valikhani <103781147+M0BIN-V@users.noreply.github.com>
2024-06-11 01:20:00 +0330

    Fix

--- Resulver/ContentResult.cs
+++ Resulver/ContentResult.cs
@@ -4,8 +4,11 @@ public abstract class ContentResult<TContent> : Result
 {
     public required TContent Content { get; set; }
 
-    public ContentResult(TContent content)
+    public ContentResult(TContent content, string? message = null) :
+        base(message)
     {
         Content = content;
     }
+
+    public ContentResult(ResultError error) : base(error) { }
 }
