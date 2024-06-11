4dd57791fe793333ef6aecb13d4896cef7e5d07e
Mobin Valikhani <103781147+M0BIN-V@users.noreply.github.com>
2024-06-11 19:15:02 +0330

    Fix

--- Resulver/IResult.cs
+++ Resulver/IResult.cs
@@ -5,6 +5,11 @@ public interface IResult
     public bool IsFailure { get; }
     public bool IsSuccess { get; }
 
-    public string? Message { get; set; }
-    public IResultError? Error { get; }
+    public string? Message { get; init; }
+    public List<IResultError> Errors { get; init; }
+}
+
+public interface IResult<TContent> : IResult
+{
+    public TContent Content { get; set; }
 }
--- Resulver/Result.cs
+++ Resulver/Result.cs
@@ -1,23 +1,30 @@
 ﻿namespace Resulver;
 
-public abstract class Result : IResult
+public class Result : IResult
 {
-    public bool IsSuccess { get; }
-    public bool IsFailure { get => !IsSuccess; }
+    public bool IsSuccess { get => Errors.Count == 0; }
+    public bool IsFailure { get => Errors.Count > 0; }
 
-    public IResultError? Error { get; }
+    public List<IResultError> Errors { get; init; } = [];
 
-    public string? Message { get; set; }
+    public string? Message { get; init; }
 
     public Result(string? message = null)
     {
         Message = message;
-        IsSuccess = true;
     }
 
     public Result(IResultError error)
     {
-        IsSuccess = false;
-        Error = error;
+        Errors.Add(error);
     }
+}
+
+public class Result<TContent> : Result, IResult<TContent>
+{
+    public required TContent Content { get; set; }
+
+    public Result(string? message = null) : base(message) { }
+
+    public Result(ResultError error) : base(error) { }
 }
